145acfe51deb461d6a49197c7fd47dad37baba91
sarahanduca <anducasarah@gmail.com>
2023-03-03 11:05:56 -0300

    add err handler

--- api.js
+++ api.js
@@ -8,14 +8,18 @@ const roundRobinQueue = new roundRobin();
 app.listen(3000, () => console.log(`Listening on port 3000`));
 
 app.get("/", async (req, res) => {
-  const server = req.headers["connection-type"]
-    ? roundRobinQueue.connection()
-    : roundRobinQueue.default();
+  try {
+    const server = req.headers["connection-type"]
+      ? roundRobinQueue.connection()
+      : roundRobinQueue.default();
 
-  if (req.headers["connection-type"]) {
-    console.log(!roundRobinQueue.connectS1 ? "S1 connection-type: TCP" : "");
-    console.log("S2 e S3 connection-type: UDP via round-robin");
+    if (req.headers["connection-type"]) {
+      console.log(!roundRobinQueue.connectS1 ? "S1 connection-type: TCP" : "");
+      console.log("S2 e S3 connection-type: UDP via round-robin");
+    }
+    const request = await axios.get(server);
+    return res.send(request.data);
+  } catch (err) {
+    console.log(err.message);
   }
-  const request = await axios.get(server);
-  return res.send(request.data);
 });
